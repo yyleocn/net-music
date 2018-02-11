4febb5bedd8b24f086bc3d54d086e8a4f1f1baab
XiaoLe <youngleocn@qq.com>
2018-02-11 11:14:59 +0800

    init net-music

---
+++ src/js/admin.js
@@ -0,0 +1,10 @@
+let APP_ID = 'XnSndmRe6ecqgsV5lz8DLCHV-gzGzoHsz';
+let APP_KEY = '4UVaXps7tlTlC3UzTAogSrwS';
+let AV = require('leancloud-storage');
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY,
+});
+
+console.log(1);
