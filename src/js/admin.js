d2e4f0bb52865e155bdf5921f2c6fc3fddfb584d
XiaoLe <youngleocn@qq.com>
2018-02-14 07:52:26 +0800

    2018-02-14

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,17 +1,8 @@
 import '../css/admin.scss';
 
-const AV = require('leancloud-storage');
 const axios = require('axios');
 const qs = require('qs');
-
-const APP_ID = 'XnSndmRe6ecqgsV5lz8DLCHV-gzGzoHsz';
-const APP_KEY = '4UVaXps7tlTlC3UzTAogSrwS';
-
-
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY,
-});
+import * as Leancloud from './leancloud.js';
 
 axios.post('/get-token.php',
     qs.stringify({
@@ -24,3 +15,5 @@ axios.post('/get-token.php',
     console.log(err_.response.data);
     console.dir(err_);
 });
+
+console.log(Leancloud);
---
+++ src/js/leancloud.js
@@ -0,0 +1,13 @@
+const AV = require('leancloud-storage');
+
+const APP_ID = 'XnSndmRe6ecqgsV5lz8DLCHV-gzGzoHsz';
+const APP_KEY = '4UVaXps7tlTlC3UzTAogSrwS';
+
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY,
+});
+export {
+    AV,
+};
