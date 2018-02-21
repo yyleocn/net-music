9043170e9e6b7a5997724bd493c0a5a47c310916
XiaoLe <youngleocn@qq.com>
2018-02-21 23:01:33 +0800

    edit qiniu-key-template

--- admin-server/qiniu-key-template.php
+++ admin-server/qiniu-key-template.php
@@ -5,3 +5,15 @@ $bucket = '';
 
 $user = '';
 $password = '';
+
+function checkLogin($user_, $password_)
+{
+    global $user,$password;
+    if ($user_ != $user) {
+        return false;
+    }
+    if ($password_ != $password) {
+        return false;
+    }
+    return true;
+}
